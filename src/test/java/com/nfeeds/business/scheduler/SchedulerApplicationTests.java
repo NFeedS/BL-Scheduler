8754acb13237633e3e6aa28f7193fe4bdf231c79
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-02-07 10:44:49 +0100

    first commit

---
+++ src/test/java/com/nfeeds/business/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfeeds.business.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
